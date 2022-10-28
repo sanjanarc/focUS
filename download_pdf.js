f1313ba88a27bf8ae8af3d144076a44f2a0d4731
sanjana roy <106613209+sanjanarc@users.noreply.github.com>
2022-10-28 17:03:46 -0500

    Delete download_pdf.js

--- download_pdf.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function() {
-    let doc = new jsPDF('p','pt','a4');
-    doc.addHTML(document.body,function() {
-        doc.save('Your Volunteer Recommendations.pdf');
-    });
-},3000);
